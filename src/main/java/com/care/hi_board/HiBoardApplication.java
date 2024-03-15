8656014bb500e8b0eebcfaee08116b72c671e213
mk520 <mk520@BOOK-MK55>
2024-03-15 17:54:09 +0900

    hi_board start

---
+++ src/main/java/com/care/hi_board/HiBoardApplication.java
@@ -0,0 +1,13 @@
+package com.care.hi_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiBoardApplication.class, args);
+	}
+
+}
